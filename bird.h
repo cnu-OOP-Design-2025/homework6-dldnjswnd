0b1d67e836e049829498a88a61dfe15debe906f8
WJ <aricatao@naver.com>
2025-11-03 22:36:41 +0900

    과제제출

--- bird.h
+++ bird.h
@@ -1,30 +1,85 @@
 #pragma once
-
 #include <iostream>
+#include <string>
 
-class Bird {
-    std::string species; 
+class Bird
+{
+protected:
+    std::string species;
+
 public:
-    Bird(){}
-    Bird(std::string species): species(species){
+    Bird() = default;
+    explicit Bird(std::string s) : species(std::move(s))
+    {
         std::cout << species << " has borned" << std::endl;
     }
 
-    virtual void display() {
+    virtual ~Bird()
+    {
+        std::cout << species << " has died" << std::endl;
+    }
+
+    virtual void display()
+    {
         std::cout << "Species: " << species << std::endl;
     }
 
-    virtual void fly() {
+    virtual void fly()
+    {
         std::cout << "This bird can fly." << std::endl;
     }
 
-    virtual void swim() {
+    virtual void swim()
+    {
         std::cout << "This bird can swim." << std::endl;
     }
 
-    virtual void cry() = 0;
+    virtual void cry() const = 0;
+    virtual void quack() const { cry(); }
+};
 
-    virtual ~Bird() {
-        std::cout << species << " has died" << std::endl;
-     }
+class Duck final : public Bird
+{
+public:
+    Duck() : Bird("Duck") { cry(); }
+    ~Duck() override { cry(); }
+
+    void cry() const override
+    {
+        std::cout << "Quack!" << std::endl;
+    }
+};
+
+class Penguin final : public Bird
+{
+public:
+    Penguin() : Bird("Penguin") { cry(); }
+    ~Penguin() override { cry(); }
+
+    void cry() const override
+    {
+        std::cout << "Squawk!" << std::endl;
+    }
+
+    void fly() override
+    {
+        std::cout << "This bird can't fly." << std::endl;
+    }
+};
+
+class Eagle final : public Bird
+{
+public:
+    Eagle() : Bird("Eagle") { cry(); }
+    ~Eagle() override { cry(); }
+
+    void cry() const override
+    {
+        std::cout << "Screech!" << std::endl;
+    }
+
+    void swim() override
+    {
+        std::cout << "This bird can't swim." << std::endl;
+    }
 };
